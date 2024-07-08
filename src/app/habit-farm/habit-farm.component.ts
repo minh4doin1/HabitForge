f595e342290a13f766232adfbb683eb0c883a32e
minh4doin1 <minh2002811@gmail.com>
2024-07-08 15:22:28 +0700

    add file

--- src/app/habit-farm/habit-farm.component.ts
+++ src/app/habit-farm/habit-farm.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-habit-farm',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './habit-farm.component.html',
   styleUrl: './habit-farm.component.css'
 })
 export class HabitFarmComponent {
+addHabit() {
+throw new Error('Method not implemented.');
+}
+togglePopup() {
+throw new Error('Method not implemented.');
+}
+plots: any;
+showPopup: any;
+newHabit: any;
+dragStart($event: DragEvent) {
+throw new Error('Method not implemented.');
+}
+allowDrop($event: DragEvent) {
+throw new Error('Method not implemented.');
+}
+drop($event: DragEvent) {
+throw new Error('Method not implemented.');
+}
 
 }
